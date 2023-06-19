9a695c861674d920a21eee8e3bb64dcbe9532ee0
vlad <cyberlord2003@gmail.com>
2023-06-19 22:38:49 +0300

    Added find_music header

---
+++ find_music.h
@@ -0,0 +1,14 @@
+#ifndef FIND_MUSIC_H
+#define FIND_MUSIC_H
+
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "dll.h"
+#include "array.h"
+
+arr_cl search_music(char* path);
+
+#endif /*FIND_MUSIC_H*/
